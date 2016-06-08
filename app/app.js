72401e4b44253a6946cdb0ab12c34dfe942291b3
Hongyi Zhang <hzhan014@ucr.ed>
2016-06-07 21:41:32 -0700

    added more stuff

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import Search from './components/search.component';
 import Details from './components/details.component';
 import Player from './components/player.component';
+import Progress from './components/progress.component';
+import Footer from './components/footer.component';
 class App extends React.Component {
 	render(){
 		return (
@@ -11,6 +13,10 @@ class App extends React.Component {
 				<Search />
 				<Details title={'Track title'} />
 				<Player />
+				<Progress position={'0.3'}
+					elapsed={'00:00'}
+					total={'0:40'}/>
+				<Footer />
 			</div>
 		);
 	}
